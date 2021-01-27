215c0b9a8e69f0760335ded84c79960b06177287
Lachlan Campbell <lachlanjc@hey.com>
2021-01-27 12:09:45 -0500

    Add Plausible

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import App from 'next/app'
+import PlausibleProvider from 'next-plausible'
 import { ThemeProvider } from 'theme-ui'
 
 import Meta from '../components/meta'
@@ -10,10 +11,12 @@ export default class extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <ThemeProvider theme={theme}>
-        <Meta />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <PlausibleProvider domain="lachlanjc.com">
+        <ThemeProvider theme={theme}>
+          <Meta />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </PlausibleProvider>
     )
   }
 }
--- pages/_document.js
+++ pages/_document.js
@@ -9,7 +9,7 @@ export default class extends Document {
 
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en-US">
         <Head />
         <body>
           <InitializeColorMode />
