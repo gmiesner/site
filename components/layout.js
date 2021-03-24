502c1e9bb87b5b2b7c20a6db5696f4394826f7ef
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-24 02:36:55 +0000

    🐍

--- components/layout.js
+++ components/layout.js
@@ -13,33 +13,6 @@ import Avatar from './avatar'
 export const ColorButton = ({ sx, ...props }) => {
   const [mode, setMode] = useColorMode()
   return (
-    <IconButton
-      {...props}
-      sx={{
-        color: 'primary',
-        borderRadius: 'circle',
-        transition: 'box-shadow .125s ease-in-out',
-        ':hover,:focus': {
-          boxShadow: '0 0 0 2px',
-          outline: 'none'
-        },
-        ...sx
-      }}
-      onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
-      title="Reverse color scheme"
-    >
-      <svg viewBox="0 0 32 32" width="24" height="24" fill="currentcolor">
-        <circle
-          cx="16"
-          cy="16"
-          r="14"
-          fill="none"
-          stroke="currentcolor"
-          strokeWidth="4"
-        />
-        <path d="M 16 0 A 16 16 0 0 0 16 32 z" />
-      </svg>
-    </IconButton>
   )
 }
 
