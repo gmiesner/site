8c433badc24ee4ab28e798246ecd9056c42bd73f
Gabriella  Miesner <ella.miesner@gmail.com>
2021-03-24 02:40:11 +0000

    👩‍💻

--- components/layout.js
+++ components/layout.js
@@ -13,6 +13,33 @@ import Avatar from './avatar'
 export const ColorButton = ({ sx, ...props }) => {
   const [mode, setMode] = useColorMode()
   return (
+    <IconButton
+      {...props}
+      sx={{
+        color: 'primary',
+        borderRadius: 'circle',
+        transition: 'box-shadow .125s ease-in-out',
+        ':hover,:focus': {
+          boxShadow: '0 0 0 2px',
+          outline: 'none'
+        },
+        ...sx
+      }}
+      onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+      title="Reverse color scheme"
+    >
+      <svg viewBox="0 0 32 32" width="24" height="24" fill="currentcolor">
+        <circle
+          cx="16"
+          cy="16"
+          r="14"
+          fill="none"
+          stroke="currentcolor"
+          strokeWidth="4"
+        />
+        <path d="M 16 0 A 16 16 0 0 0 16 32 z" />
+      </svg>
+    </IconButton>
   )
 }
 
@@ -34,12 +61,6 @@ export const Header = () => (
       <NavLink>Résumé</NavLink>
     </Link>
     <Box sx={{ mx: 'auto' }} />
-    <ColorButton
-      onClick={e => {
-        const next = mode === 'dark' ? 'light' : 'dark'
-        setMode(next)
-      }}
-    />
   </Container>
 )
 
